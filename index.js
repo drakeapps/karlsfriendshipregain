4c9f94ba097175603398d6d974b291b93a42fd9d
James Wilson <drakeapps@users.noreply.github.com>
2021-04-07 13:10:47 -0500

    Update index.js
    Co-authored-by: Jade Michael Thornton <jademichael@jmthornton.net>

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const interval = setInterval(() => {
 		return clearInterval(interval);
 	}
 
-	let particleCount = 50 * (timeLeft / duration);
+	const particleCount = 50 * (timeLeft / duration);
 	// since particles fall down, start a bit higher than random
 	confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
 	confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
