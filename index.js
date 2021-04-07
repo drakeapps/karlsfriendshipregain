d4f75654789d39a1d9915d7e72d81e7f35c05143
James Wilson <james.wilson@flightaware.com>
2021-04-07 13:03:19 -0500

    More es6 ways

--- index.js
+++ index.js
@@ -34,18 +34,18 @@ var duration = 60 * 1000;
 var animationEnd = Date.now() + duration;
 var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-function randomInRange(min, max) {
-  return Math.random() * (max - min) + min;
-}
-var interval = setInterval(function() {
-  var timeLeft = animationEnd - Date.now();
+const randomInRange = (min, max) => {
+	return Math.random() * (max - min) + min;
+};
+const interval = setInterval(() => {
+	var timeLeft = animationEnd - Date.now();
 
-  if (timeLeft <= 0) {
-    return clearInterval(interval);
-  }
+	if (timeLeft <= 0) {
+		return clearInterval(interval);
+	}
 
-  var particleCount = 50 * (timeLeft / duration);
-  // since particles fall down, start a bit higher than random
-  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
-  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+	let particleCount = 50 * (timeLeft / duration);
+	// since particles fall down, start a bit higher than random
+	confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+	confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
 }, 500);
